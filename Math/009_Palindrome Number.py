08acaa2ef232eb42a8ed28aa83fcc587d9c4cb95
Min-Sian, Su <minsiansu@gmail.com>
2018-07-12 01:30:32 +0800

    update

--- Math/009_Palindrome Number.py
+++ Math/009_Palindrome Number.py
@@ -8,6 +8,7 @@ class Solution:
         :rtype: bool
         """
         return str(x) == str(x)[::-1]
-   
-if __name__ == "__main__":    
+
+
+if __name__ == "__main__":
     print(Solution().isPalindrome(1221))
