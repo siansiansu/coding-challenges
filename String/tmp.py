eb0f7e20ab52184440131289ce9803f5944c136b
Min-Sian, Su <minsiansu@gmail.com>
2018-07-13 21:20:47 +0800

    update

---
+++ String/tmp.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        for i, j in zip(*strs):
+            if 
