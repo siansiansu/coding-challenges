dc1fe556b3fc3eb3a812a32e9cf7ba2b932365f8
Min-Sian, Su <minsiansu@gmail.com>
2018-07-14 01:38:08 +0800

    leetcode practice

--- String/014_Longest_Common_Prefix.py
+++ String/014_Longest_Common_Prefix.py
@@ -2,13 +2,25 @@
 # -*- coding: utf-8 -*-
 
 class Solution:
-    def longestCommonPrefix(self, strs):
+    def longestCommonPrefix1(self, strs):
+        if not strs: return ""
+        
+        res = ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i > len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+
+    def longestCommonPrefix3(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-            return ''
+        if not strs: return ''
+
         for i, j in enumerate(zip(*strs)):
             if len(set(j)) > 1:
                 return strs[0][:i]
@@ -16,7 +28,5 @@ class Solution:
 
 
 if __name__ == "__main__":
-    print(Solution().longestCommonPrefix(['abde', 'ab', 'ab']))
-
-
-
+    strs = ["flower","flow","flight"] 
+    print(Solution().longestCommonPrefix1(strs))
--- String/tmp.py
+++ String/tmp.py
@@ -5,5 +5,16 @@ class Solution:
         :rtype: str
         """
         if not strs: return ""
-        for i, j in zip(*strs):
-            if 
+        
+        res = ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i > len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+if __name__ == "__main__":
+    strs = ["flower","flow","flight"]
+    strs2 = ["dog","racecar","car"]
+    print(Solution().longestCommonPrefix(strs))
