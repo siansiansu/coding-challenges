a91911bf6741fb4aea78c4f15383fd62dfe3872f
Alex Su <minsiansu@gmail.com>
2019-01-20 16:22:24 +0800

    update files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LeetCode-Problems
+![GitHub](https://img.shields.io/github/license/mashape/apistatus.svg) 
 
 coding practice
---
+++ tests/test_codility.py
@@ -0,0 +1,4 @@
+from codility import iterations_binarygap
+
+def test_binary_gap():
+    assert iterations_binarygap.solution(1041) == 5
