fa379792cd2cea1a9e2124d696006bd3a618b1a7
Alex Su <minsiansu@gmail.com>
2019-02-01 19:20:45 +0800

    update files

--- tests/test_codility.py
+++ tests/test_codility.py
@@ -4,6 +4,7 @@ from codility import cyclic_rotation
 from codility import frog_jmp
 from codility import perm_missing_elem
 from codility import tape_equilibrium
+from codility import perm_check
 
 def test_binary_gap():
     ans1 = binary_gap.solution(1041)
@@ -28,4 +29,8 @@ def test_perm_missing_elem():
 
 def test_tape_equilibrium():
     ans1 = tape_equilibrium.solution([3, 1, 2, 4, 3])
+    assert ans1 == 1
+
+def test_perm_check():
+    ans1 = perm_check.solution([4, 1, 3, 2])
     assert ans1 == 1
