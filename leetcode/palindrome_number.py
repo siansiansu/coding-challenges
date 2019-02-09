38b3829475123c4e3b10c93a71ac0f2e466980f0
siansiansu <minsiansu@gmail.com>
2019-02-09 21:02:43 +0800

    update files

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -1,14 +1,2 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-class Solution:
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        return str(x) == str(x)[::-1]
-
-
-if __name__ == "__main__":
-    print(Solution().isPalindrome(1221))
+def isPalindrome(self, x: 'int') -> 'bool':
+    return str(x) == str(x)[::-1]         # 使用 Python slice 的技巧，如果倒著的數字也一樣的話則返回 True。
--- leetcode/reverse_integer.py
+++ leetcode/reverse_integer.py
@@ -1,19 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        sign = (x>0)-(x<0)
-        x = sign * int(str(abs(x))[::-1])
-        x = 0 if abs(x) > 0x7FFFFFFF else x
-        
-        return x
-
-if __name__ == "__main__":
-    print (Solution().reverse(123))
-    print (Solution().reverse(-123))
-    print (Solution().reverse(120))
+def reverse(self, x: 'int') -> 'int':
+    sign = (x>0)-(x<0)
+    
+    if abs(x) > 0x7FFFFFFF:
+        return 0
+    else:
+        return sign * int(str(abs(x))[::-1])
