d1f3af067df7b352ab2f497685306827de80f004
siansiansu <minsiansu@gmail.com>
2019-02-11 04:07:35 +0800

    update files

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -1,2 +1,4 @@
-def isPalindrome(self, x: 'int') -> 'bool':
-    return str(x) == str(x)[::-1]         # 使用 Python slice 的技巧，如果倒著的數字也一樣的話則返回 True。
+class Solution(object):
+    def isPalindrome(self, x):
+        # 使用 Python slice 的技巧，如果倒著的數字也一樣的話則返回 True。
+        return str(x) == str(x)[::-1]
--- leetcode/reverse_integer.py
+++ leetcode/reverse_integer.py
@@ -1,7 +1,8 @@
-def reverse(self, x: 'int') -> 'int':
-    sign = (x>0)-(x<0)                        # 判斷正負號。
-    
-    if abs(x) > 0x7FFFFFFF:                   # 超過大小限制 return 0。
-        return 0
-    else:
-        return sign * int(str(abs(x))[::-1])  # 將數字反轉過來並加上正負號。
+class Solution(object):
+    def reverse(self, x):
+        sign = (x > 0)-(x < 0)                        # 判斷正負號。
+
+        if abs(x) > 0x7FFFFFFF:                   # 超過大小限制 return 0。
+            return 0
+        else:
+            return sign * int(str(abs(x))[::-1])  # 將數字反轉過來並加上正負號。
