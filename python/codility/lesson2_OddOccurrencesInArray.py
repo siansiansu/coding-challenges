770f54676f38a1aa991dd81cdf3b42d7f15c44c3
siansiansu <minsiansu@gmail.com>
2019-02-11 23:21:39 +0800

    update files

--- python/codility/lesson2_OddOccurrencesInArray.py
+++
@@ -1,9 +0,0 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-def solution(A):
-    # write your code in Python 3.6
-    res = 0
-    for num in A:
-        res ^= num
-    return res
