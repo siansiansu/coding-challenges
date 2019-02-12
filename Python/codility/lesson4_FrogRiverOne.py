457ca5631b47993bf2002f89d2672fde75f9bf14
siansiansu <minsiansu@gmail.com>
2019-02-12 18:35:32 +0800

    update files

--- Python/codility/lesson4_FrogRiverOne.py
+++ Python/codility/lesson4_FrogRiverOne.py
@@ -1,12 +1,12 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+
 def solution(X, A):
     # write your code in Python 3.6
-    S = set()
-    count = len(A)
-    for i in range(0, count):
-        S.add(A[i])
-        if(len(S) == X):
+    s = set()
+    for i, j in enumerate(A):
+        s.add(j)
+        if len(s) == X:
             return i
     return -1
