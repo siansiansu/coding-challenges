476d37d791b0e45a57110c861b0896c5ee6b88c8
Alex Su <minsiansu@gmail.com>
2019-07-11 02:32:32 +0800

    [FEATURE] Two Sum in JavaScript

--- leetcode/javascript/001_two_sum.js
+++ leetcode/javascript/001_two_sum.js
@@ -3,15 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-
-
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
     let hashTable = {};
-    for (let i = 0; i < nums.length; i++) {
-        if (hashTable[target - nums[i]] > 0) {
+    for(let i = 0; i < nums.length; i++) {
+        if(hashTable[target - nums[i]] != undefined) {
             return [hashTable[target - nums[i]], i];
         } else {
-            hashTable[nums[i]] = i;
+            hashTable[nums[i]] = i;    
         };
     };
 };
